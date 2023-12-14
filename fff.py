f23d5869b21996de8bb39579fc54589adb3fc229
1anttree1 <valera.gaz.sh@gmail.com>
2023-12-14 10:09:16 +0300

    rf

--- fff.py
+++ fff.py
@@ -1,2 +1,9 @@
-for i in range(1):
-    print('ff')
+def ff(n,s):
+    if n == s:
+        return 1
+    if n>s or n == 23:
+        return 0
+    return ff(n+2, s) + ff(n*3, s) + ff(n*5, s)
+print(ff(1,13)* ff(13,75))
+for i in range(1,101):
+    print("")
