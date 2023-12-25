1af0c8755afa575b37e0d2b4d31dc82f6040074a
1anttree1 <valera.gaz.sh@gmail.com>
2023-12-25 12:38:43 +0300

    Merge branch 'main' of https://github.com/1anttree1/ege

--- 25/fip25 var1.py
+++ 25/fip25 var1.py
@@ -1,4 +1,4 @@
 from fnmatch import *
-for i in range(0, 10**10+1, 31007):
-    if fnmatch(str(i), '1*34?5?9'):
-        print(i, i//31007)
+for i in range(0, 10**8, 2025):
+    if fnmatch(str(i), '12*34?5'):
+        print(i, i//2025)
