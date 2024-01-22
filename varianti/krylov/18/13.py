589a81d54b123658f5f81110e260381a1899de66
1anttree1 <valera.gaz.sh@gmail.com>
2024-01-22 12:57:06 +0300

    аа

--- varianti/krylov/18/13.py
+++ varianti/krylov/18/13.py
@@ -2,6 +2,6 @@ from ipaddress import *
 for i in range(1, 32):
     net = ip_network(f'244.55.138.100/{i}', False)
     if str(net.network_address) == '240.0.0.0':
-        print(net.network_address)
+        print(net)
 
 #/цифра это количество едениц
